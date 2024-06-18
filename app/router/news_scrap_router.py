44eda45b76dff05c5472dc40450487bf378fcbab
pykido <animoto1@naver.com>
2024-06-18 17:02:38 +0900

    feat(news_scrap_router.py) : 기사 스크래핑 API 라우터 구현

--- app/router/news_scrap_router.py
+++ app/router/news_scrap_router.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from app.model.article_model import ArticleResponse
+from app.service.news_scrap_service import extract_article
 from app.service.crawl_article_service import extract_article
-from app.service.crawl_mk import extract_article_mk
-from app.service.crawl_hk import extract_article_hk
-from app.service.crawl_sedaily import extract_article_sedaily
 
-news_scrap_rotuer = APIRouter()
+news_scrapping_router = APIRouter()
 
-@news_scrap_rotuer.get("/extract-article/{news_type}", response_model=ArticleResponse)
+@news_scrapping_router.get("/extract-article", response_model=ArticleResponse)
+async def extract_article_api(url: str):
+    return await extract_article(url)
+
+@news_scrapping_router.get("/extract-article/{news_type}", response_model=ArticleResponse)
 async def extract_article_api(news_type: str, url: str):
     return await extract_article(news_type, url)
