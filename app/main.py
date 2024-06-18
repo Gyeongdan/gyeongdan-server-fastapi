65f70ed7be22732acd7be1ea1f7a73ddbeb64ebc
pykido <animoto1@naver.com>
2024-06-18 17:03:27 +0900

    feat(main.py) : main에 라우터 추가

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.config.exception_handler import exception_handler, http_exception_handl
 from app.config.middlewares.request_response_logging_middle_ware import (
     LoggingMiddleware,
 )
+from app.router.news_scrap_router import news_scrapping_router
 from app.router.article_crud_router import articles_router
 from app.router.news_scrap_router import news_scrap_rotuer
 
@@ -22,6 +23,8 @@ app.include_router(articles_router)
 app.add_exception_handler(Exception, exception_handler)
 app.add_exception_handler(HTTPException, http_exception_handler)
 
+# news_scrapping_router를 app에 추가
+app.include_router(news_scrapping_router)
 
 @app.get("/")
 async def root():
