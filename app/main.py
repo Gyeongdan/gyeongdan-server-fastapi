65508810a8321983e02398ddc7e5abf7a9907982
정지민 <108014449+stopmin@users.noreply.github.com>
2024-06-22 01:12:46 +0900

    exception handler 적용 (#11)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from starlette.exceptions import HTTPException
 
+from app.config.exception_handler import exception_handler, http_exception_handler
 from app.config.middlewares.request_response_logging_middle_ware import (
     LoggingMiddleware,
 )
@@ -11,9 +13,14 @@ app.add_middleware(LoggingMiddleware)
 
 app.include_router(article_crud_router.router)
 
+app.add_exception_handler(Exception, exception_handler)
+app.add_exception_handler(HTTPException, http_exception_handler)
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
