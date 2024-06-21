0545beb2d58dd225d06fd4ccef1637db33dc0454
pykido <animoto1@naver.com>
2024-06-22 02:13:42 +0900

    feat(crawl_article_service.py) : 매일경제, 한국경제, 서울경제 기사 제목 및 본문 내용 크롤링 한 파일에 통합

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 from app.config.middlewares.request_response_logging_middle_ware import (
     LoggingMiddleware,
 )
-from app.router.news_scrap_router import news_scrapping_router
+from app.router.news_scrap_router import news_scrap_rotuer
 
 app = FastAPI()
 
 app.add_middleware(LoggingMiddleware)
 
 # news_scrapping_router를 app에 추가
-app.include_router(news_scrapping_router)
+app.include_router(news_scrap_rotuer)
 
 @app.get("/")
 async def root():
--- app/router/news_scrap_router.py
+++ app/router/news_scrap_router.py
@@ -1,9 +1,24 @@
 from fastapi import APIRouter
 from app.model.article_model import ArticleResponse
-from app.service.news_scrap_service import extract_article
+from app.service.crawl_article_service import extract_article
+from app.service.crawl_mk import extract_article_mk
+from app.service.crawl_hk import extract_article_hk
+from app.service.crawl_sedaily import extract_article_sedaily
 
-news_scrapping_router = APIRouter()
+news_scrap_rotuer = APIRouter()
 
-@news_scrapping_router.get("/extract-article", response_model=ArticleResponse)
-async def extract_article_api(url: str):
-    return await extract_article(url)
+@news_scrap_rotuer.get("/extract-article/{news_type}", response_model=ArticleResponse)
+async def extract_article_api(news_type: str, url: str):
+    return await extract_article(news_type, url)
+
+@news_scrap_rotuer.get("/mk_news", response_model=ArticleResponse)
+async def extract_article_api_mk(url: str):
+    return await extract_article_mk(url)
+
+@news_scrap_rotuer.get("/hk_news", response_model=ArticleResponse)
+async def extract_article_api_hk(url: str):
+    return await extract_article_hk(url)
+
+@news_scrap_rotuer.get("/sedaily_news", response_model=ArticleResponse)
+async def extract_article_api_sedaily(url: str):
+    return await extract_article_sedaily(url)
