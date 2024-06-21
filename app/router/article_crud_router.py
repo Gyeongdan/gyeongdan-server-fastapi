5179024c169ebe7177cc23bd3a79a842a364db39
stopmin <geemin0@naver.com>
2024-06-22 02:02:53 +0900

    inclue articles_router

--- app/router/article_crud_router.py
+++ app/router/article_crud_router.py
@@ -1,76 +1,101 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from app.model.crawled_article_model import CrawledArticle, CrawledArticleCreate, CrawledArticleUpdate, \
-    CrawledArticleSchema
-from app.database.repository import DatabaseRepository, get_repository
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.session import get_db_session
 from app.service.crawled_article_service import ArticleService
 
-router = APIRouter()
+articles_router = APIRouter()
 
-def get_article_service(repo: DatabaseRepository[CrawledArticle] = Depends(get_repository(CrawledArticle))) -> ArticleService:
-    return ArticleService(repo)
 
-# 1. Create a new article
-@router.post("/articles", response_model=CrawledArticleSchema, status_code=status.HTTP_201_CREATED)
+class ArticleCreateRequestDTO(BaseModel):
+    url: str
+    publisher: str
+
+
+@articles_router.post("/articles")
 async def create_article(
-    article: CrawledArticleCreate,
-    service: ArticleService = Depends(get_article_service)
+    dto: ArticleCreateRequestDTO, session: AsyncSession = Depends(get_db_session)
 ):
-    try:
-        return await service.create_article(article)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+    return await ArticleService().create_article(dto.url, dto.publisher, session)
 
 
-# 2. READ articles
-@router.get("/articles", response_model=list[CrawledArticleSchema], status_code=status.HTTP_200_OK)
-async def read_all_articles(
-    service: ArticleService = Depends(get_article_service)
-):
-    try:
-        return await service.get_all_articles()
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Unexpected error")
-
-@router.get("/articles/{article_id}", response_model=CrawledArticleSchema, status_code=status.HTTP_200_OK)
-async def read_article_by_id(
-    article_id: int,
-    service: ArticleService = Depends(get_article_service)
-):
-    try:
-        article = await service.get_article_by_id(article_id)
-        if article is None:
-            raise HTTPException(status_code=404, detail="Article not found")
-        return article
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-# Update an article by ID
-@router.put("/articles/{id}", response_model=CrawledArticleSchema, status_code=status.HTTP_200_OK)
-async def update_article(
-    id: int,
-    article: CrawledArticleUpdate,
-    service: ArticleService = Depends(get_article_service)
-):
-    try:
-        updated_article = await service.update_article(id, article)
-        if updated_article is None:
-            raise HTTPException(status_code=404, detail="Article not found")
-        return updated_article
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-# Delete an article by ID
-@router.delete("/articles/{id}", response_model=None, status_code=status.HTTP_204_NO_CONTENT)
-async def delete_article(
-    id: int,
-    service: ArticleService = Depends(get_article_service)
-):
-    try:
-        await service.delete_article(id)
-    except Exception as e:
-        print(f"Error occurred: {e}")
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+#
+# def get_article_service(repo: DatabaseRepository[CrawledArticle] = Depends(
+#     get_repository(CrawledArticle))) -> ArticleService:
+#     return ArticleService(repo)
+#
+#
+# # 1. Create a new article
+# @router.post("/articles", response_model=CrawledArticleSchema,
+#              status_code=status.HTTP_201_CREATED)
+# async def create_article(
+#     article: CrawledArticleCreate,
+#     service: ArticleService = Depends(get_article_service)
+# ):
+#     try:
+#         return await service.create_article(article)
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
+#
+#
+# # 2. READ articles
+# @router.get("/articles", response_model=list[CrawledArticleSchema],
+#             status_code=status.HTTP_200_OK)
+# async def read_all_articles(
+#     service: ArticleService = Depends(get_article_service)
+# ):
+#     try:
+#         return await service.get_all_articles()
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Unexpected error")
+#
+#
+# @router.get("/articles/{article_id}", response_model=CrawledArticleSchema,
+#             status_code=status.HTTP_200_OK)
+# async def read_article_by_id(
+#     article_id: int,
+#     service: ArticleService = Depends(get_article_service)
+# ):
+#     try:
+#         article = await service.get_article_by_id(article_id)
+#         if article is None:
+#             raise HTTPException(status_code=404, detail="Article not found")
+#         return article
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
+#
+#
+# # Update an article by ID
+# @router.put("/articles/{id}", response_model=CrawledArticleSchema,
+#             status_code=status.HTTP_200_OK)
+# async def update_article(
+#     id: int,
+#     article: CrawledArticleUpdate,
+#     service: ArticleService = Depends(get_article_service)
+# ):
+#     try:
+#         updated_article = await service.update_article(id, article)
+#         if updated_article is None:
+#             raise HTTPException(status_code=404, detail="Article not found")
+#         return updated_article
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
+#
+#
+# # Delete an article by ID
+# @router.delete("/articles/{id}", response_model=None,
+#                status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_article(
+#     id: int,
+#     service: ArticleService = Depends(get_article_service)
+# ):
+#     try:
+#         await service.delete_article(id)
+#     except Exception as e:
+#         print(f"Error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
--- app/service/crawled_article_service.py
+++ app/service/crawled_article_service.py
@@ -1,38 +1,30 @@
-from typing import List, Optional
-from app.model.crawled_article_model import CrawledArticle, CrawledArticleCreate, CrawledArticleUpdate
-from app.database.repository import DatabaseRepository
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.model.article_publisher import find_publisher
+from app.model.crawled_article import CrawledArticle
+from app.repository.crawled_article_crud import CrawledArticleRepository
+
 
 class ArticleService:
-    def __init__(self, repo: DatabaseRepository[CrawledArticle]):
-        self.repo = repo
+    async def create_article(
+        self, url: str, publisher: str, session: AsyncSession
+    ) -> CrawledArticle:
+        return await CrawledArticleRepository().create(
+            article=CrawledArticle(
+                url=url,
+                title="테스트입니당",
+                content="테스트입니다",
+                publisher=find_publisher(publisher).name,
+            ),
+            session=session,
+        )
 
-    async def create_article(self, article_data: CrawledArticleCreate) -> CrawledArticle:
-        article_dict = article_data.dict()
-        article_dict['newspaper'] = article_dict['newspaper'].value  # Enum to string conversion
-        article_dict['regeneration_model'] = article_dict['regeneration_model'].value  # Enum to string conversion
-        return await self.repo.create(article_dict)
+    async def get_article_by_id(
+        self, article_id: int, session: AsyncSession
+    ) -> CrawledArticle:
+        return await CrawledArticleRepository().get(pk=article_id, session=session)
 
-    async def get_article_by_id(self, article_id: int) -> Optional[CrawledArticle]:
-        article = await self.repo.get(article_id)
-        if article:
-            article.newspaper = article.newspaper.value  # Enum to string conversion
-            article.regeneration_model = article.regeneration_model.value  # Enum to string conversion
-        return article
-
-    async def get_all_articles(self) -> List[CrawledArticle]:
-        articles = await self.repo.filter()
-        for article in articles:
-            article.newspaper = article.newspaper.value  # Enum to string conversion
-            article.regeneration_model = article.regeneration_model.value  # Enum to string conversion
-        return articles
-
-    async def update_article(self, article_id: int, article_data: CrawledArticleUpdate) -> Optional[CrawledArticle]:
-        article_dict = article_data.dict(exclude_unset=True)
-        if 'newspaper' in article_dict and article_dict['newspaper']:
-            article_dict['newspaper'] = article_dict['newspaper'].value  # Enum to string conversion
-        if 'regeneration_model' in article_dict and article_dict['regeneration_model']:
-            article_dict['regeneration_model'] = article_dict['regeneration_model'].value  # Enum to string conversion
-        return await self.repo.update_by_pk(article_id, article_dict)
-
-    async def delete_article(self, article_id: int) -> None:
-        await self.repo.delete(article_id)
+    async def get_all_articles(self, session: AsyncSession) -> List[CrawledArticle]:
+        return await CrawledArticleRepository().get_all(session=session)
