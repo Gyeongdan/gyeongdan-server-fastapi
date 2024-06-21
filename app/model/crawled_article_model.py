4f43e765defaba39428ff0934edddaee0d41ee5e
pykido <animoto1@naver.com>
2024-06-21 16:40:52 +0900

    feat : 재생성 완료 여부 및 모델 사용 종류 추가, created_at, updated_at 에러 해결

--- app/model/crawled_article_model.py
+++ app/model/crawled_article_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, func, DateTime, Enum as SQLAEnum, Text
+from sqlalchemy import Column, Integer, String, DateTime, Enum as SQLAEnum, Text, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from pydantic import BaseModel, validator
 from enum import Enum
@@ -8,20 +8,26 @@ from datetime import datetime
 Base = declarative_base()
 
 class NewspaperEnum(Enum):
-    매일경제 = '매일경제'
-    한국경제 = '한국경제'
-    서울경제 = '서울경제'
+    MK = "MK"  # 매일경제
+    HK = "HK"  # 한국경제
+    SK = "SK"  # 서울경제
+
+class RegenerationModelEnum(Enum):
+    LLAMA3 = 'LLAMA3'
+    GPT4 = 'GPT4'
 
 class CrawledArticle(Base):
     __tablename__ = "crawled_articles"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    newspaper = Column(SQLAEnum(NewspaperEnum), nullable=False)
     url = Column(String, unique=True, nullable=False)
-    created_at = Column(DateTime, server_default=func.now(), nullable=False)
-    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
-    newspaper = Column(SQLAEnum(NewspaperEnum), nullable=False)
+    is_regenerated = Column(Boolean, default=False, nullable=False)
+    regeneration_model = Column(SQLAEnum(RegenerationModelEnum), nullable=False)
     regenerated_content = Column(Text, nullable=True)
 
 class CrawledArticleBase(BaseModel):
@@ -30,6 +36,8 @@ class CrawledArticleBase(BaseModel):
     content: str
     newspaper: NewspaperEnum
     regenerated_content: Optional[str] = None
+    is_regenerated: bool = False
+    regeneration_model : RegenerationModelEnum
 
     @validator('newspaper', pre=True)
     def validate_newspaper(cls, value):
@@ -57,6 +65,5 @@ class CrawledArticleSchema(CrawledArticleBase):
     id: int
     created_at: datetime
     updated_at: datetime
-
     class Config:
         from_attributes = True  # Pydantic V2 설정
