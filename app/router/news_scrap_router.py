68cb95044b36014c033fd65c73722e6c53e62df2
pykido <animoto1@naver.com>
2024-06-22 20:54:15 +0900

    refactor : 필요 없는 파일들 삭제

--- app/router/news_scrap_router.py
+++ app/router/news_scrap_router.py
@@ -10,15 +10,3 @@ news_scrap_rotuer = APIRouter()
 @news_scrap_rotuer.get("/extract-article/{news_type}", response_model=ArticleResponse)
 async def extract_article_api(news_type: str, url: str):
     return await extract_article(news_type, url)
-
-@news_scrap_rotuer.get("/mk_news", response_model=ArticleResponse)
-async def extract_article_api_mk(url: str):
-    return await extract_article_mk(url)
-
-@news_scrap_rotuer.get("/hk_news", response_model=ArticleResponse)
-async def extract_article_api_hk(url: str):
-    return await extract_article_hk(url)
-
-@news_scrap_rotuer.get("/sedaily_news", response_model=ArticleResponse)
-async def extract_article_api_sedaily(url: str):
-    return await extract_article_sedaily(url)
