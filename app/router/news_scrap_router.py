bd2883c9b9d570265fc4303b17b7bf6b66b4e281
정지민 <108014449+stopmin@users.noreply.github.com>
2024-06-23 14:06:16 +0900

    테스트 commit (#16)
    * 테스트 commit
    
    * 테스트 commit
    
    테스트 commit

--- app/router/news_scrap_router.py
+++ app/router/news_scrap_router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
+
 from app.model.article_model import ArticleResponse
 from app.service.crawl_article_service import extract_article
-from app.service.crawl_mk import extract_article_mk
-from app.service.crawl_hk import extract_article_hk
-from app.service.crawl_sedaily import extract_article_sedaily
 
 news_scrap_rotuer = APIRouter()
+
 
 @news_scrap_rotuer.get("/extract-article/{news_type}", response_model=ArticleResponse)
 async def extract_article_api(news_type: str, url: str):
