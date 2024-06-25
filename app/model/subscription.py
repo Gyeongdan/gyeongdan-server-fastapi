00c41bfe5ee5a10736e8f6adde0377da30483364
mandu <jys0972@gmail.com>
2024-06-25 22:40:42 +0900

    feat(subscription.py) : Enum 구현 시작 임포트, filter를 위한 index 설정

--- app/model/subscription.py
+++ app/model/subscription.py
@@ -4,6 +4,12 @@ from sqlalchemy import CHAR, BigInteger, Column, DateTime, String, event, Boolea
 
 from app.database.repository import Base
 
+from enum import Enum
+
+
+# class MailTypeCategory(Enum):
+
+
 
 class Subscription(Base):
     __tablename__ = "subscriptions"
@@ -11,12 +17,9 @@ class Subscription(Base):
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
     name = Column(CHAR(255), nullable=False)
     email_address = Column(String, nullable=False)
-    category = Column(SmallInteger, nullable=False)
-    status = Column(Boolean, nullable=False)  # true -> active , false -> unSubscribe
+    category = Column(SmallInteger, index=True, nullable=False)
+    status = Column(Boolean, index=True, nullable=False)  # true -> active , false -> unSubscribe
     created_at = Column(DateTime, default=datetime.now, nullable=False)
-
-
-
 
 @event.listens_for(Subscription, "before_update", propagate=True)
 def update_timestamp(mapper, connection, target):  # pylint: disable=unused-argument
