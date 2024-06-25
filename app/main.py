902f780a1884f08d6d142326da6aad933f7e01c6
mandu <jys0972@gmail.com>
2024-06-25 11:28:35 +0900

    feat(main.py) : app에 subscription_router 추가

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.config.middlewares.request_response_logging_middle_ware import (
 )
 from app.router.article_crud_router import articles_router
 from app.router.news_scrap_router import news_scrap_rotuer
+from app.router.subscription_crud_router import subscription_router
 
 app = FastAPI()
 
@@ -16,6 +17,7 @@ app.add_middleware(LoggingMiddleware)
 # routers
 app.include_router(news_scrap_rotuer)
 app.include_router(articles_router)
+app.include_router(subscription_router)
 
 
 # exception handlers
