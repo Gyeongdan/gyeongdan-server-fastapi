ee007c840fe9096239d34f8be3a8b5cebce4d4ff
stopmin <geemin0@naver.com>
2024-06-27 15:26:41 +0900

    크롤링 한 기사 저장 작업

--- app/router/news_scrap_router.py
+++ app/router/news_scrap_router.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from groq import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.database.session import get_db_session
 from app.model.article_model import ArticleResponse
 from app.service.crawl_article_service import extract_article
 from app.utils.generic_response import GenericResponseDTO
@@ -16,11 +18,15 @@ class ArticleCreateRequestDTO(BaseModel):
 @news_scrap_rotuer.post(
     "/extract-article/{news_type}", response_model=GenericResponseDTO[ArticleResponse]
 )
-async def extract_article_api(articleCreateRequestDTO: ArticleCreateRequestDTO):
+async def extract_article_api(
+    articleCreateRequestDTO: ArticleCreateRequestDTO,
+    session: AsyncSession = Depends(get_db_session),
+):
     article = await extract_article(
-        news_type=articleCreateRequestDTO.publisher, url=articleCreateRequestDTO.url
+        news_type=articleCreateRequestDTO.publisher,
+        url=articleCreateRequestDTO.url,
+        session=session,
     )
-
     return GenericResponseDTO(
         data=article, message="Article extracted successfully", result=True
     )
