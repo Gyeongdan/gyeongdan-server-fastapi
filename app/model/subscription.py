a9e4dfb5357f9abe79bad562fc26856e0c8530a1
mandu <jys0972@gmail.com>
2024-06-28 17:20:55 +0900

    feat(subscription.py) : MailTypeCategory enum으로 구현

--- app/model/subscription.py
+++ app/model/subscription.py
@@ -1,14 +1,23 @@
 from datetime import datetime
+from enum import Enum
 
-from sqlalchemy import CHAR, BigInteger, Column, DateTime, String, event, Boolean, SmallInteger
+from sqlalchemy import CHAR, BigInteger, Boolean, Column, DateTime, String, event
 
 from app.database.repository import Base
 
-from enum import Enum
 
-
-# class MailTypeCategory(Enum):
-
+class MailTypeCategory(Enum):
+    ECONOMY = "Economy"
+    BUSINESS = "Business"
+    SOCIETY = "Society"
+    INTERNATIONAL = "International"
+    REAL_ESTATE = "Real Estate"
+    STOCKS = "Stocks"
+    POLITICS = "Politics"
+    IT_SCIENCE = "IT & Science"
+    CULTURE = "Culture"
+    OPINION = "Opinion"
+    SPORTS = "Sports"
 
 
 class Subscription(Base):
@@ -17,9 +26,10 @@ class Subscription(Base):
     id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
     name = Column(CHAR(255), nullable=False)
     email_address = Column(String, nullable=False)
-    category = Column(SmallInteger, nullable=False)
+    category = Column(CHAR(255), nullable=False)
     status = Column(Boolean, nullable=False)  # true -> active , false -> unSubscribe
     created_at = Column(DateTime, default=datetime.now, nullable=False)
+
 
 @event.listens_for(Subscription, "before_update", propagate=True)
 def update_timestamp(mapper, connection, target):  # pylint: disable=unused-argument
