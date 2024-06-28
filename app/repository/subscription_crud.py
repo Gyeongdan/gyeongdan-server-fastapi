ac154d6ba0b2b5f45612e4dc6a5aff1db2635707
mandu <jys0972@gmail.com>
2024-06-28 17:51:40 +0900

    feat(subscriptions_crud.py) : Category enum으로 통일

--- app/repository/subscription_crud.py
+++ app/repository/subscription_crud.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.database.repository import get_repository, model_to_dict
-from app.model.subscription import Subscription
+from app.model.subscription import MailTypeCategory, Subscription
+
 
 class SubscriptionRepository:
     async def create(self, subscription: Subscription, session: AsyncSession):
@@ -18,21 +20,23 @@ class SubscriptionRepository:
             )
         return subscription
 
-    async def update_category(self, id: int, category: int, session: AsyncSession):
+    async def update_category(
+        self, id: int, category: MailTypeCategory, session: AsyncSession
+    ):
         repository = get_repository(Subscription)(session)
-        return await repository.update_by_pk(
-            pk=id, data={"category": category}
-        )
+        return await repository.update_by_pk(pk=id, data={"category": category.name})
 
     async def update_status(self, id: int, status: bool, session: AsyncSession):
         repository = get_repository(Subscription)(session)
-        return await repository.update_by_pk(
-            pk=id, data={"status": status}
-        )
+        return await repository.update_by_pk(pk=id, data={"status": status})
 
-    async def get_active_subscriptions_by_category(self, category: int, session: AsyncSession):
+    async def get_active_subscriptions_by_category(
+        self, category: MailTypeCategory, session: AsyncSession
+    ):
         repository = get_repository(Subscription)(session)
-        return await repository.filter(and_(Subscription.status == True, Subscription.category == category))
+        return await repository.filter(
+            and_(Subscription.status, Subscription.category == category.name)
+        )
 
     async def get_all(self, session: AsyncSession):
         repository = get_repository(Subscription)(session)
