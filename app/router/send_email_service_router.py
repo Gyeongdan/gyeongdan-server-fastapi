6fecf79e29cb38734c9d71ad0902e3b8d2103906
Vackam <dldyghks123@naver.com>
2024-06-30 19:29:44 +0900

    router 구현

--- app/router/send_email_service_router.py
+++ app/router/send_email_service_router.py
@@ -1,57 +1,48 @@
 # send_email_service_router.py
+
 from datetime import datetime, timedelta
-from typing import List
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# 활용을 위하여
 from app.database.session import get_db_session
-
-# 내가 만든 db 가져오기
+from app.model.subscription import MailTypeCategory
 from app.service.newsletter_article_service import NewsletterService
-
-# 윤성이가 만든 emailservice
 from app.service.send_email_service import SendEmailService
-
-# 윤성이가 만든 subscription db 가져오기
 from app.service.subscription_service import SubscriptionService
 
 send_email_service_router = APIRouter()
 
-# email을 string으로 받을 지 email로 받을 지
-# category는 Enum으로 바꿔야 할 듯
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USER = "your_email@email.com"  # 예시입니다!
+SMTP_PASSWORD = "PASSWORD_PLEASE"  # 예시입니다!
 
-
-class SendEmailContent(BaseModel):
-    category: int
-    contents: List[str]
-    email: List[str]
-
-
-# 우선 내껄로 테스트
-sender = SendEmailService(
-    "smtp.gmail.com", 587, "jys0972@gmail.com", "ktoc hgtz fzpm mbcg"
-)
+sender = SendEmailService(SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD)
 
 
 @send_email_service_router.post("/send-email/")
-async def send_email(category: int, session: AsyncSession = Depends(get_db_session)):
+async def send_email(
+    category: MailTypeCategory, session: AsyncSession = Depends(get_db_session)
+):
     # email_list 받아오기
     subscriptions = await SubscriptionService().get_active_subcriptions_by_category(
         category, session
     )
     email_list = [subscription.email_address for subscription in subscriptions]
-    print(email_list)
 
     # news_letter 받아오기
-    # 시간 기준은 함수 실행
     time_now = datetime.now()
     news_letters = await NewsletterService().get_content_by_category(category, session)
     letter_list = [
         news_letter.content
         for news_letter in news_letters
+        # 기준을 12시간 안에 기사가 생성됐냐 안됐냐로 따졌습니다. 이는 조정하면 될 것 같습니다!
         if news_letter.updated_at >= (time_now - timedelta(hours=12))
     ]
+
+    # 기사를 카테고리 별로 보낼 때, 하나의 주제는 하나의 메일로 보내는 것이 좋을 듯 하여 이런 코드를 짰습니다!
+    news_article_zip = "\n\n".join(letter_list)
+
+    await sender.send_email_to_users(category.value, news_article_zip, email_list)
     return letter_list
