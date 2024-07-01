3d1e9868211f0a0f72fc850f9b658a1d2566a561
정지민 <108014449+stopmin@users.noreply.github.com>
2024-07-02 01:41:54 +0900

    DB에서 메일 정보, 기사 받아서 보내기 (#40)
    * 기사 재생성 LLM 요청 프로토타입 #25 (#36)
    
    * 환경변수 파일 ignore 추가
    
    * python-dotenv 의존성 추가
    - .env 파일
    
    * get_platform_client 생성
    
    * 네이밍 수정
    
    * 쉬운 기사 변환 프로토타입 생성
    
    * 프롬프트 버저닝 및 txt파일로 분리
    
    * ai 요청 결과를 json으로 파싱
    
    * 링크를 넣으면 크롤링 해서 새로운 쉬운 기사로 생성하여 DB에 저장
    
    * email 전송을 위한 router 파일 추가
    
    * rebasing
    
    * router 구현
    
    * 매직넘버 Enum으로 수정
    
    ---------
    
    Co-authored-by: Vackam <dldyghks123@naver.com>

--- app/router/article_crud_router.py
+++ app/router/article_crud_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.session import get_db_session
-from app.service.crawled_article_service import ArticleService
+from app.service.article_manage_service import ArticleManageService
 
 articles_router = APIRouter()
 
@@ -11,16 +11,16 @@ articles_router = APIRouter()
 async def update_simplified_content(
     id: int, simplified_content: str, session: AsyncSession = Depends(get_db_session)
 ):
-    return await ArticleService().update_simplified_article(
+    return await ArticleManageService().update_simplified_article(
         id=id, simplified_content=simplified_content, session=session
     )
 
 
 @articles_router.get("/articles/{id}")
 async def get_article_by_id(id: int, session: AsyncSession = Depends(get_db_session)):
-    return await ArticleService().get_article_by_id(id, session)
+    return await ArticleManageService().get_article_by_id(id, session)
 
 
 @articles_router.get("/articles")
 async def get_all_articles(session: AsyncSession = Depends(get_db_session)):
-    return await ArticleService().get_all_articles(session)
+    return await ArticleManageService().get_all_articles(session)
