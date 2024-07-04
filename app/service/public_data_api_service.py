7be2655056f9a6b6a8d95059727a55f1a587aa02
mandu <jys0972@gmail.com>
2024-07-05 01:34:58 +0900

    feat(public_data_api_service.py) : 공공데이터 api 호출하는 서비스 구현

---
+++ app/service/public_data_api_service.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from typing import List
+
+import httpx
+
+
+class PublicDataAPIInfo:
+    def __init__(self, api_name: str, url: str, urlElements: List[str]) -> None:
+        self.api_name = api_name
+        self.url = url
+        self.urlElements = urlElements
+
+
+class PublicDataAPI(Enum):
+    population = PublicDataAPIInfo(
+        api_name="인구",
+        url="https://apis.data.go.kr/1240000/IndicatorService/IndListSearchRequest",
+        urlElements=["pageNo=1", "numOfRows=10", "STAT_JIPYO_NM=인구", "format=json"],
+    )
+    youth_policy = PublicDataAPIInfo(
+        api_name="청소년 정책",
+        url="https://apis.data.go.kr/1383000/yhis/YouthNewsService/getYouthNewsList",
+        urlElements=["pageNo=1", "numOfRows=10", "type=xml&pstRegYmd=20231101"],
+    )
+
+
+class PublicDataAPIService:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    async def response(self, publicDataApi: PublicDataAPIInfo):
+        url = publicDataApi.url + "?" + "serviceKey=" + self.api_key
+        for urlElement in publicDataApi.urlElements:
+            url += "&" + urlElement
+
+        async with httpx.AsyncClient(verify=False) as client:
+            response = await client.get(url)
+        return response.text
