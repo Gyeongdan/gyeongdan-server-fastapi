c9f86697e98f9cdf3a642f45504f52a982abf81b
stopmin <geemin0@naver.com>
2024-07-13 17:43:45 +0900

    ChromaDBManager 추가

---
+++ app/articles/rag_lang_chain/chromadb_manager.py
@@ -0,0 +1,27 @@
+import os
+from typing import List
+
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+
+class ChromaDBManager:
+    def __init__(self):
+        openai_api_key = os.getenv("OPENAI_API_KEY")
+        self.embeddings = OpenAIEmbeddings(api_key=openai_api_key)
+        self.vectorstore = Chroma(
+            collection_name="documents", embedding_function=self.embeddings
+        )
+
+    def add_documents(self, documents: List[dict]):
+        texts = [doc["snippet"] for doc in documents]
+        metadata = [{"id": doc["link"], "title": doc["title"]} for doc in documents]
+        self.vectorstore.add_texts(texts, metadata)
+
+    def search_documents(self, query: str, k: int = 5) -> List[dict]:
+        query_vector = self.embeddings.embed_query(query)
+        results = self.vectorstore.similarity_search_by_vector(query_vector, k=k)
+        return results
