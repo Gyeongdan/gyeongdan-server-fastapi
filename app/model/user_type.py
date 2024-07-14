37ba66cea2fc86329a366bed648c0f636cd044ca
stopmin <geemin0@naver.com>
2024-07-14 18:05:38 +0900

    UserTypes 위치 수정

--- app/model/user_type.py
+++ app/model/user_type.py
@@ -1,7 +1,8 @@
+from enum import Enum
+
 from sqlalchemy import BigInteger, Column, Integer
 
 from app.database.repository import Base
-
 
 
 class UserType(Base):
@@ -14,3 +15,11 @@ class UserType(Base):
     user_type_tech_specialist = Column(Integer, nullable=True)
     user_type_professionals = Column(Integer, nullable=True)
 
+
+class UserTypes(Enum):
+    NONE: -1
+    ISSUE_FINDER: 0
+    LIFESTYLE_CONSUMER: 1
+    ENTERTAINER: 2
+    TECH_SEPCIALIST: 3
+    PROFESSIONALS: 4
