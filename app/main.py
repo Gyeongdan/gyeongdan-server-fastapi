fce948a337c58b6b57ff907526758840ac68ea2c
mandu <jys0972@gmail.com>
2024-07-14 18:41:37 +0900

    refact(main.py) : 사용하지 않는 라우터 제거

--- app/main.py
+++ app/main.py
@@ -9,10 +9,8 @@ from app.config.middlewares.request_response_logging_middle_ware import (
     LoggingMiddleware,
 )
 from app.router.generate_simple_article_router import simple_article_router
-from app.router.newsletter_article_crud_router import newsletter_article_router
-from app.router.send_email_service_router import send_email_service_router
-from app.router.subscription_crud_router import subscription_router
 from app.router.user_type_router import user_type_router
+from app.router.send_email_service_router import send_email_service_router
 from app.service.news_scheduling_service import schedule_task
 
 app = FastAPI()
@@ -30,11 +28,9 @@ load_dotenv()
 app.add_middleware(LoggingMiddleware)
 
 # routers
-app.include_router(subscription_router)
-app.include_router(newsletter_article_router)
-app.include_router(send_email_service_router)
 app.include_router(user_type_router)
 app.include_router(simple_article_router)
+app.include_router(send_email_service_router)
 
 # exception handlers
 app.add_exception_handler(Exception, exception_handler)
