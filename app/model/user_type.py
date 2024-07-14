33d86dc48a8ed692a2b9fe86c8569e6ccd744d4c
mandu <jys0972@gmail.com>
2024-07-14 16:05:27 +0900

    refact(user_type.py) : user_id로 변경

--- app/model/user_type.py
+++ app/model/user_type.py
@@ -7,7 +7,7 @@ from app.database.repository import Base
 class UserType(Base):
     __tablename__ = "user_type"
 
-    id = Column(BigInteger, primary_key=True, index=True)
+    user_id = Column(BigInteger, primary_key=True, index=True)
     user_type_issue_finder = Column(Integer, nullable=True)
     user_type_lifestyle_consumer = Column(Integer, nullable=True)
     user_type_entertainer = Column(Integer, nullable=True)
