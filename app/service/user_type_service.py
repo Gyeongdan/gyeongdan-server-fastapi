ffbe245f98660d99d80ca0278a716f8202f18375
mandu <jys0972@gmail.com>
2024-07-14 16:03:34 +0900

    refact(user_type_service.py) : UserTypes Enum으로 관리

--- app/service/user_type_service.py
+++ app/service/user_type_service.py
@@ -9,6 +9,15 @@ from app.model.user_type import UserType
 from app.repository.user_type_crud import UserTypeRepository
 from enum import Enum
 
+class UserTypes(Enum):
+    NONE : -1
+    ISSUE_FINDER : 0
+    LIFESTYLE_CONSUMER : 1
+    ENTERTAINER : 2
+    TECH_SEPCIALIST :3
+    PROFESSIONALS : 4
+
+
 class UserTypeQuestionnaireRequestDTO(BaseModel):
     id: int
     answers: List[int]
@@ -54,6 +63,8 @@ def calculate_user_type(answers:UserTypeQuestionnaireRequestDTO, questionnaire_d
     for i in range(10):#answer in answers.answers:
         answer_index = int(answers.answers[i])  # answers의 각 항목을 정수로 변환
         user_type_index = int(questionnaire_data[i][answer_index+1][2])  # 정수 인덱스로 접근
+        if (user_type_index == UserTypes.NONE):
+            continue
         user_type[user_type_index] += questionnaire_data[i][answer_index+1][1]
     return user_type
 
@@ -62,54 +73,54 @@ class UserTypeService:
     def __init__(self):
         self.questionnaire_data = [
             ['최신 경제 이슈에 대해 얼마나  잘 알고 있습니까?',
-             ['매우 잘 알고 있다.', 10, 0],
-             ['다소 알고 있다.', 5, 0],
-             ['잘 모른다.', 0, -1],
+             ['매우 잘 알고 있다.', 10, UserTypes.ISSUE_FINDER.value],
+             ['다소 알고 있다.', 5, UserTypes.ISSUE_FINDER.value],
+             ['잘 모른다.', 0, UserTypes.NONE.value],
              ],
             ['경제 뉴스를 얼마나 자주 찾아보십니까?',
-             ['매일 확인한다.', 10, 0],
-             ['주간 단위로 확인한다.', 5, 0],
-             ['가끔 확인한다.', 0, -1],
+             ['매일 확인한다.', 10, UserTypes.ISSUE_FINDER.value],
+             ['주간 단위로 확인한다.', 5, UserTypes.ISSUE_FINDER.value],
+             ['가끔 확인한다.', 0, UserTypes.NONE.value],
              ],
             ['경제 관련 논란이나 논쟁에 얼마나 관심이 있습니까?',
-             ['매우 관심이 있다.', 10, 0],
-             ['다소 관심이 있다.', 5, 0],
-             ['잘 모른다.', 0, -1],
+             ['매우 관심이 있다.', 10, UserTypes.ISSUE_FINDER.value],
+             ['다소 관심이 있다.', 5, UserTypes.ISSUE_FINDER.value],
+             ['잘 모른다.', 0, UserTypes.NONE.value],
              ],
             ['경제 정보를 어떻게 활용하시나요?',
-             ['일상 생활에 적용해본다.', 10, 1],
-             ['흥미로운 정보는 기억한다.', 10, 2],
-             ['크게 활용하지 않는다.', 0, -1],
+             ['일상 생활에 적용해본다.', 10, UserTypes.LIFESTYLE_CONSUMER.value],
+             ['흥미로운 정보는 기억한다.', 10, UserTypes.ENTERTAINER.value],
+             ['크게 활용하지 않는다.', 0, UserTypes.NONE.value],
              ],
             ['절약이나 소비자 팁에 관심이 있으신가요?',
-             ['매우 관심이 있다.', 10, 2],
-             ['다소 관심이 있다.', 5, 2],
-             ['별로 관심이 없다.', 0, -1],
+             ['매우 관심이 있다.', 10, UserTypes.LIFESTYLE_CONSUMER.value],
+             ['다소 관심이 있다.', 5, UserTypes.LIFESTYLE_CONSUMER.value],
+             ['별로 관심이 없다.', 0, UserTypes.NONE.value],
              ],
             ['경제 관련 이야기를 어떻게 즐기시나요?',
-             ['심도 깊게 분석한다.', 10, 4],
-             ['가벼운 마음으로 즐긴다.', 5, 2],
-             ['별로 관심이 없다.', 0, -1],
+             ['심도 깊게 분석한다.', 10, UserTypes.PROFESSIONALS.value],
+             ['가벼운 마음으로 즐긴다.', 5, UserTypes.ENTERTAINER.value],
+             ['별로 관심이 없다.', 0, UserTypes.NONE.value],
              ],
             ['기술과 경제의 결합에 대해 얼마나 잘 이해하고 있습니까?',
-             ['매우 잘 이해한다.', 10, 3],
-             ['다소 이해한다.', 5, 3],
-             ['잘 모른다.', 0, -1],
+             ['매우 잘 이해한다.', 10, UserTypes.TECH_SEPCIALIST.value],
+             ['다소 이해한다.', 5, UserTypes.TECH_SEPCIALIST.value],
+             ['잘 모른다.', 0, UserTypes.NONE.value],
              ],
             ['기술 발전이 경제에 미치는 영향에 대해 얼마나 알고 있습니까?',
-             ['깊이 있는 지식이 있다. ', 10, 3],
-             ['일반적인 이해만 한다. ', 5, 3],
-             ['잘 모른다.', 0, -1],
+             ['깊이 있는 지식이 있다. ', 10, UserTypes.TECH_SEPCIALIST.value],
+             ['일반적인 이해만 한다. ', 5, UserTypes.TECH_SEPCIALIST.value],
+             ['잘 모른다.', 0, UserTypes.NONE.value],
              ],
             ['전문가 의견이나 통계 데이터에 관심이 있으신가요?',
-             ['매우 관심이 있다.', 10, 4],
-             ['다소 관심이 있다.', 5, 4],
-             ['별로 관심이 없다. ', 5, 2],
+             ['매우 관심이 있다.', 10, UserTypes.PROFESSIONALS.value],
+             ['다소 관심이 있다.', 5, UserTypes.PROFESSIONALS.value],
+             ['별로 관심이 없다. ', 5, UserTypes.ENTERTAINER.value],
              ],
             ['경제 분석을 얼마나 자주 읽거나 들으시나요?',
-             ['자주 읽거나 듣는다.', 10, 4],
-             ['가끔 읽거나 듣는다.', 5, 4],
-             ['별로 읽거나 듣지 않는다.', 5, 2],
+             ['자주 읽거나 듣는다.', 10, UserTypes.PROFESSIONALS.value],
+             ['가끔 읽거나 듣는다.', 5, UserTypes.PROFESSIONALS.value],
+             ['별로 읽거나 듣지 않는다.', 5, UserTypes.ENTERTAINER.value],
              ]
         ]
     async def get_questionnaire(self) -> List[UserTypeQuestionnaire]:
