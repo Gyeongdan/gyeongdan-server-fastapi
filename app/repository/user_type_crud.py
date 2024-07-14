ef284abc3c306a87c0a3a84eed01a3989a2e8a88
정지민 <108014449+stopmin@users.noreply.github.com>
2024-07-14 18:07:56 +0900

    UserTypes 위치 수정 (#94)

--- app/repository/user_type_crud.py
+++ app/repository/user_type_crud.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from fastapi import HTTPException
-from sqlalchemy import and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.repository import get_repository, model_to_dict
@@ -18,21 +17,27 @@ class UserTypeRepository:
         repository = get_repository(UserType)(session)
         user_type = await repository.get(pk)
         if user_type is None:
-            raise HTTPException(
-                status_code=404, detail="해당 유저가 없습니다."
-            )
+            raise HTTPException(status_code=404, detail="해당 유저가 없습니다.")
         return user_type
 
-    async def update_user_type(self, id: int, user_types:List[int], session: AsyncSession):
+    async def update_user_type(
+        self, id: int, user_types: List[int], session: AsyncSession
+    ):
         repository = get_repository(UserType)(session)
-        return await repository.update_by_pk(pk=id, data={
-            'user_type_issue_finder': user_types[UserTypes.ISSUE_FINDER.value],
-            'user_type_lifestyle_consumer': user_types[UserTypes.LIFESTYLE_CONSUMER.value],
-            'user_type_entertainer': user_types[UserTypes.ENTERTAINER.value],
-            'user_type_tech_specialist': user_types[UserTypes.TECH_SEPCIALIST.value],
-            'user_type_professionals': user_types[UserTypes.PROFESSIONALS.value]
-        })
-
+        return await repository.update_by_pk(
+            pk=id,
+            data={
+                "user_type_issue_finder": user_types[UserTypes.ISSUE_FINDER.value],
+                "user_type_lifestyle_consumer": user_types[
+                    UserTypes.LIFESTYLE_CONSUMER.value
+                ],
+                "user_type_entertainer": user_types[UserTypes.ENTERTAINER.value],
+                "user_type_tech_specialist": user_types[
+                    UserTypes.TECH_SEPCIALIST.value
+                ],
+                "user_type_professionals": user_types[UserTypes.PROFESSIONALS.value],
+            },
+        )
 
     async def get_all(self, session: AsyncSession):
         repository = get_repository(UserType)(session)
--- app/router/user_type_router.py
+++ app/router/user_type_router.py
@@ -1,25 +1,27 @@
-import asyncio
 from typing import List
 
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.session import get_db_session
-from app.model.user_type import UserType
-from app.service.user_type_service import UserTypeService, UserTypeQuestionnaire
+from app.service.user_type_service import (
+    UserTypeQuestionnaire,
+    UserTypeQuestionnaireRequestDTO,
+    UserTypeService,
+)
 from app.utils.generic_response import GenericResponseDTO
-from app.service.user_type_service import UserTypeQuestionnaireRequestDTO
 
 user_type_router = APIRouter()
 
-@user_type_router.get("/user-type/questionnaire", response_model=GenericResponseDTO[List[UserTypeQuestionnaire]])
+
+@user_type_router.get(
+    "/user-type/questionnaire",
+    response_model=GenericResponseDTO[List[UserTypeQuestionnaire]],
+)
 async def get_questionnaire():
     data = await UserTypeService().get_questionnaire()
     return GenericResponseDTO[List[UserTypeQuestionnaire]](
-        data= data,
-        message="Questionnaire retrieved successfully",
-        result=True
+        data=data, message="Questionnaire retrieved successfully", result=True
     )
 
 
@@ -28,10 +30,7 @@ async def create_subscription(
     request: UserTypeQuestionnaireRequestDTO,
     session: AsyncSession = Depends(get_db_session),
 ):
-    await UserTypeService().create_user_type(
-        request, session
-    )
+    await UserTypeService().create_user_type(request, session)
     return GenericResponseDTO[int](
         data=request.id, message="user type created successfully", result=True
     )
-
