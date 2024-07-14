6d31530fa8981bba5b5ce9dffb88f519cbaf8a7c
mandu <jys0972@gmail.com>
2024-07-14 15:50:20 +0900

    feat(user_type_router.py) : 유저 분류 테이블 router 구현

---
+++ app/router/user_type_router.py
@@ -0,0 +1,37 @@
+import asyncio
+from typing import List
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.database.session import get_db_session
+from app.model.user_type import UserType
+from app.service.user_type_service import UserTypeService, UserTypeQuestionnaire
+from app.utils.generic_response import GenericResponseDTO
+from app.service.user_type_service import UserTypeQuestionnaireRequestDTO
+
+user_type_router = APIRouter()
+
+@user_type_router.get("/user_type/questionnaire", response_model=GenericResponseDTO[List[UserTypeQuestionnaire]])
+async def get_questionnaire():
+    data = await UserTypeService().get_questionnaire()
+    return GenericResponseDTO[List[UserTypeQuestionnaire]](
+        data= data,
+        message="Questionnaire retrieved successfully",
+        result=True
+    )
+
+
+@user_type_router.post("/user_Type/save", response_model=GenericResponseDTO[int])
+async def create_subscription(
+    request: UserTypeQuestionnaireRequestDTO,
+    session: AsyncSession = Depends(get_db_session),
+):
+    await UserTypeService().create_user_type(
+        request, session
+    )
+    return GenericResponseDTO[int](
+        data=request.id, message="user type created successfully", result=True
+    )
+
