911e69f380c0f4462bc2be48227e8d5df5ad7edd
Vackam <dldyghks123@naver.com>
2024-07-16 23:27:56 +0900

    router enum 추가, get 함수 추가

--- app/router/api_visualization_router.py
+++ app/router/api_visualization_router.py
@@ -1,11 +1,21 @@
+from enum import Enum
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.session import get_db_session
-from app.service.api_visualization_service import create_article
-from app.service.public_data_api_service import PublicDataAPI
+from app.service.api_visualization_service import (
+    ApiVisualizationService,
+    create_article,
+)
 
 api_visualization_router = APIRouter()
+
+
+class select_data(Enum):
+    population = "인구"
+    childhood = "청소년 정책"
+    JINJU = "진주 코비드"
 
 
 # 대충 이렇게 해놓고 모델 이런 거 만들어야 겠다.
@@ -13,12 +23,20 @@ api_visualization_router = APIRouter()
 async def api_visualization_article(
     user_input: bool,
     title: str,
-    data_set: PublicDataAPI,
+    data_set: select_data,
     session: AsyncSession = Depends(get_db_session),
 ):
     # 지금 api 에서 고른다고 가정
     await create_article(
-        title=title, data=data_set, user_input=user_input, session=session
+        title=title, data=data_set.value, user_input=user_input, session=session
     )
 
-    return "잘 되너 듯"
+    return "Great JOB"
+
+
+@api_visualization_router.get("/api_visual/article/{id}")
+async def get_api_visualization_article(
+    id: int,
+    session: AsyncSession = Depends(get_db_session),
+):
+    return await ApiVisualizationService().get_by_id(id=id, session=session)
