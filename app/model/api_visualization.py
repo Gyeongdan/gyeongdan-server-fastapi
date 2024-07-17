a0adec91081c53f81026b33924ea009eb26dae91
Vackam <dldyghks123@naver.com>
2024-07-17 17:29:01 +0900

    model에 args 추가

--- app/model/api_visualization.py
+++ app/model/api_visualization.py
@@ -7,6 +7,7 @@ from app.database.repository import Base
 
 class ApiVisualization(Base):
     __tablename__ = "api_visualization"
+    __table_args__ = {"schema": "gyeongdan"}
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)  # 고유 식별자
     title = Column(String, nullable=False)  # 제목
