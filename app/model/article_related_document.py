610d873e853e5bfd0be3242a95634a3fb3122047
김태윤 <77539625+pykido@users.noreply.github.com>
2024-07-18 05:15:07 +0900

    Rag를 통해 생성되는 related_documents DB에 저장하기 (#117)
    * feat : 지민이가 만든 RAG/LangChanin 활용하여 [경단챗봇 - 기사 API] 구현
    
    * feat : article_related_document 테이블 짜기
    
    * feat : related_documents DB에 저장하기
    
    * feat : 리팩토링 - 1
    
    * feat : 리팩토링 - 2
    
    * feat : 하 드디어 끝
    
    * 지민이 리뷰 반영

---
+++ app/model/article_related_document.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Text
+from sqlalchemy.orm import relationship
+
+from app.database.repository import Base
+
+class ArticleRelatedDocument(Base):
+    __tablename__ = 'article_related_documents'
+    __table_args__ = {'schema': 'gyeongdan'}
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    article_id = Column(Integer, ForeignKey('gyeongdan.articles.id', ondelete='CASCADE'))
+    title = Column(String(255), nullable=False)
+    link = Column(String(255), nullable=True)
+    snippet = Column(Text)
+
+    article = relationship("Articles", back_populates="related_documents")
